2bc81032773eb2da8298fcbe1f29e9a6e07bb11c
Matt Mecham <msmecham.dev@gmail.com>
2022-04-07 14:35:45 -0600

    Fixed typo

--- law_of_cosines.f90
+++ law_of_cosines.f90
@@ -6,11 +6,11 @@ program law_of_cosine
     REAL s1, s2, radian, degree, s3 !Variables used to calculate length of third side of a triangle
 
     PRINT *, "This program uses the law of cosine to find the length of a side of a triangle."
-    PRINT*, "Please enter the lengths of two sides: "
+    PRINT*, "Please enter the lengths of two sides:"
     READ*, s1, s2
-    PRINT*, "Now the angle in between in decimal radians: "
+    PRINT*, "Now the angle in between in decimal radians:"
     READ*, radian
     degree = radian * (180/PI)
-    s3 = sqrt(s1**2 + s2**2 - 2*s1*s2*cos(radian)) !Notice that sqrt, power, and trig are all implicit functions of Fortran
-    PRINT*, "The angle between in degrees is: ", degree, "and the length of the third side is", s3, "units long"
+    s3 = sqrt(s1**2 + s2**2 - 2*s1*s2*cos(radian)) !Notice that sqrt, power, and trig are all intrinsic functions of Fortran
+    PRINT*, "The angle between in degrees is:", degree, "and the length of the third side is", s3, "units long"
 end program
