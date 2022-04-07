d4aad1d48a1dc80e1333a29f38a19e5ff8a73f99
jakebeslanowitch <jakebeslanowitch@u.boisestate.edu>
2022-04-07 14:18:53 -0600

    Implemented HexagonArea Calculator

---
+++ HexagonArea.f90
@@ -0,0 +1,21 @@
+! Jake Beslanowitch
+! HexagonArea Calculator
+PROGRAM  HexagonArea
+    IMPLICIT  NONE
+    
+    REAL  :: sideLength, area, input
+    DOUBLE PRECISION:: n
+
+    WRITE(*,*)  'Enter one side of the trapezoid : '
+    READ(*,*)    sideLength
+    Input = 3
+    n = SQRT(input)
+    n = (n * 3)/2
+    area = (sideLength**2) * n
+
+    WRITE(*,*)  'Entered values:'
+    WRITE(*,*)  'One side  = ', sideLength
+    WRITE(*,*)
+    WRITE(*,*)  'Area Calculated = ', area
+
+END PROGRAM  HexagonArea
