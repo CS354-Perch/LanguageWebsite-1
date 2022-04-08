0d9fefb070504aced4e53395947430c01e0c9379
Sam W <61138994+samwise-311@users.noreply.github.com>
2022-04-07 20:19:09 -0600

    Samuel Wasko - Simple Program

--- TrapezoidArea.f90
+++ TrapezoidArea.f90
@@ -1,21 +1,25 @@
 PROGRAM  TrapezoidArea
+!   Program computes the area of a trapezoid
+!   after gathering three real dimensions from user input
+!   @author Samuel Wasko
     IMPLICIT  NONE
 
     REAL  :: Base1, Base2, Height
     REAL  :: Area, t
 
+    ! gather dimensions from user input
     WRITE(*,*)  'Enter height of trapezoid : '
     READ(*,*)   Height
-
     WRITE(*,*)  'Enter 1st base length of trapezoid : '
     READ(*,*)   Base1
-
     WRITE(*,*)  'Enter 2nd base length of trapezoid : '
     READ(*,*)   Base2
 
+    ! use temp variables to compute area
     t      = (Base1+Base2)/2
     Area   = Height * t * 1
 
+    ! write statements for computation output
     WRITE(*,*)  'Entered values:'
     WRITE(*,*)  'Height = ', Height
     WRITE(*,*)  'Base1 = ', Base1
