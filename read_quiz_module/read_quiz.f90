8ab97162bcf31b851c6d96fe94fb1bc5c3b78b0d
Matt Mecham <msmecham.dev@gmail.com>
2022-04-19 14:22:42 -0600

    updated this to be implicit none

--- read_quiz_module/read_quiz.f90
+++ read_quiz_module/read_quiz.f90
@@ -1,15 +1,15 @@
-!Matt Mecham - CS354
+!Matt Mecham - CS356
 !Module that takes two allocatable arrays,
 !Reads from a file that has alternating lines of questions and answers
 !Stores the questions in A1 and the answer in A2
 MODULE read_quiz
+IMPLICIT NONE
 
 CONTAINS
     SUBROUTINE read_questions(A1, A2)
-        IMPLICIT NONE
-        CHARACTER*128, DIMENSION(:), ALLOCATABLE, INTENT(INOUT) :: A1 !Declares that A1 param is an array, is going to be dynamic, and has read/write permissions
-        INTEGER, DIMENSION(:), ALLOCATABLE, INTENT(INOUT) :: A2 !Same as A1
-        character*128 :: string !Assumes no line in file is longer than 128 chars. Fortran strings are as bad as C
+        CHARACTER*128, DIMENSION(:), ALLOCATABLE, INTENT(INOUT) :: A1
+        INTEGER, DIMENSION(:), ALLOCATABLE, INTENT(INOUT) :: A2
+        CHARACTER*128 :: string !Assumes no line in file is longer than 128 chars. Fortran strings are as bad as C
         INTEGER :: file_id = 1 !Fortran uses integer values to refer to files. Calling it file_id and assigning it 1 makes it more readable
         INTEGER :: read_error = 0 !Fortran uses a flag when reading files for errors. If it encounters an error when reading, it will assign
         INTEGER :: i = 0, num_lines = 0 !Loop variables
@@ -29,13 +29,13 @@ CONTAINS
 
         !This loop reads through our file line by line. If the modulo of i is odd, then it is a question and is stored in A1
         !If modulo of i is even, then it is an answer and stored in A2
-        !Since the file is getting split into two arrays, the indices of each are operated on accordingly to make sure that the questoin/answer
+        !Since the file is getting split into two arrays, the indices of each are operated on accordingly to make sure that the question/answer
         !pairs have the same indices between the two arrays
         DO i = 1, num_lines
             IF (MODULO(i, 2) == 1) THEN
                 READ(file_id,'(A)') A1(i/2 + 1) !read(<file id #>, <format>) variable - In this case, '(A)' tells it to read character*n - basically, the end of the line
             ELSE
-                READ(file_id, *) A2(i/2) !The format specifier here, *, states "Read until first whitespace or end of line*. It auto type casts to int since that is what the array is
+                READ(file_id, *) A2(i/2) !The format modifier, *, in this case indicates to read until first white space or end of line. It will type cast to int
             END IF
         END DO
 
