a17bcfdc89b576ad2ea3c987a912df8a2396111e
jakebeslanowitch <jakebeslanowitch@u.boisestate.edu>
2022-04-21 10:12:37 -0600

    Updated comments

--- FibonacciSequence.f90
+++ FibonacciSequence.f90
@@ -1,5 +1,6 @@
 ! Jake Beslanowitch
 ! Calcuates the Fibonacci Sequence up until the 45th number
+! 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, and so on
 PROGRAM FibonacciSequence
     IMPLICIT NONE
     INTEGER :: X, Y, TEMP, ITR
@@ -7,11 +8,12 @@ PROGRAM FibonacciSequence
     Y = 1 ! start with base case 
     WRITE (*,*) X ! write the value of x
     WRITE (*,*) Y ! write the value of y
-    ! iterate until 45
-    DO ITR = 1, 45, 1
-          TEMP = X + Y
-          X = Y
-          Y = TEMP
+    ! do: initial value, final value, increment value
+    ! if no increment is set then default to 1
+    DO ITR = 1, 45, 1       ! iterate until 45
+          TEMP = X + Y      ! create temp variable with base cases 
+          X = Y             ! assign right val with left val
+          Y = TEMP          ! right val is now the value of tmp
           WRITE (*,*) TEMP
        END DO
 END PROGRAM FibonacciSequence
--- HexagonArea.f90
+++ HexagonArea.f90
@@ -1,18 +1,24 @@
 ! Jake Beslanowitch
 ! HexagonArea Calculator
 PROGRAM  HexagonArea
+    ! all variables starting with i, j, k, l, m and n, if
+    ! not declared, are of the INTEGER type by default.
     IMPLICIT  NONE
-    
     REAL  :: sideLength, area, input
+    ! works the same as real
     DOUBLE PRECISION:: n
-
+    ! enter the size of one side
     WRITE(*,*)  'Enter one side of the trapezoid : '
+    ! second asterisk makes the read statement list-directed.
     READ(*,*)    sideLength
+    ! ((3 * SQRT(3)) / 2) * a^2
     input = 3
     n = SQRT(input)
     n = (n * 3)/2
     area = (sideLength**2) * n
 
+    ! Write will send items to a numbered unit, which can be a
+    screen
     WRITE(*,*)  'Entered values:'
     WRITE(*,*)  'One side  = ', sideLength
     WRITE(*,*)
